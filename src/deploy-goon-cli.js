f3f26fe204e3c50941aa34ec31cf785554abfa28
Matt Farmer <matt@frmr.me>
2013-10-25 10:58:13 -0400

    Some work on fleshing out the cli structure.

--- src/deploy-goon-cli.js
+++ src/deploy-goon-cli.js
@@ -9,4 +9,57 @@
 
 process.title = "deploygoon";
 
-console.log("I am not a teapot.");
+var acceptedCommands = {
+      add: {
+        helpfulName: "deploygoon add path/to/deploygoon.json",
+        description: "Add a project to deploy goon.",
+        callback: function() {}
+      },
+      remove: {
+        helpfulName: "deploygoon remove project-slug",
+        description: "Remove a project from deploy goon.",
+        callback: function() {}
+      },
+      ls: {
+        helpfulName: "deploygoon ls",
+        description: "List all projects deploy goon knows about.",
+        callback: function() {}
+      },
+      start: {
+        helpfulName: "deploygoon start",
+        description: "Start the deploy goon daemon.",
+        callback: function() {}
+      },
+      stop: {
+        helpfulName: "deploygoon stop",
+        description: "Stop the deploy goon daemon.",
+        callback: function() {}
+      },
+      usage: {
+        helpfulName: "deploygoon usage",
+        description: "Display this usage information.",
+        callback: usageCommand
+      }
+    },
+    arguments = process.argv,
+    command = ifValidCommand(arguments.shift()) || "usage";
+
+function ifValidCommand(command) {
+  if (acceptedCommands.hasOwnProperty(command))
+    command
+  else
+    undefined
+}
+
+function usageCommand() {
+  console.log("deploygoon -- A goon for deployment.");
+  console.log("");
+
+  for (command in acceptedCommands) {
+    console.log(acceptedCommands[command].helpfulName);
+    console.log("\t" + acceptedCommands[command].description);
+    console.log("");
+  }
+}
+
+acceptedCommands[command].callback();
