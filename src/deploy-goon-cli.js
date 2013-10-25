3d20114be72b65a623e4c20226a8ebfa03696d44
Matt Farmer <matt@frmr.me>
2013-10-24 23:27:53 -0400

    Stub out a file for what will actually be the cli.

---
+++ src/deploy-goon-cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/**
+ * Deploy Goon
+ * ©2013 Matt Farmer – Licensed under Apache 2 License
+ * See LICENSE in project root for more details.
+ *
+ * Deploy Goon CLI.
+**/
+
+process.title = "deploygoon";
+
+console.log("I am not a teapot.");
