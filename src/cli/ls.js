0d4abcbb49d43fd9cae1d192e646ea8ba4c59937
Matt Farmer <matt@frmr.me>
2013-10-27 17:53:06 -0400

    Implement the ls cli command.

--- src/cli/ls.js
+++ src/cli/ls.js
@@ -4,5 +4,17 @@
  * See LICENSE in project root for more details.
 **/
 module.exports = function(arguments) {
-  console.log("Hi, I'm the ls command.");
+  var DeployGoonConfiguration = require("../util/deploy-goon-configuration"),
+      configuration = new DeployGoonConfiguration(),
+      jobs = configuration.getJobs(),
+      jobFiles = configuration.getJobFilePaths();
+
+  for (slug in jobs) {
+    var job = jobs[slug],
+        description = job.getDescription(),
+        path = jobFiles[slug];
+
+    console.log(slug + " (" + path + ")");
+    console.log("\t" + description);
+  }
 }
