f4af546cf71c9bc0e61a72cfae0193eb7a433bfb
Matt Farmer <matt@frmr.me>
2013-10-27 18:08:42 -0400

    Implement rudamentary add command for cli.

--- src/cli/add.js
+++ src/cli/add.js
@@ -4,5 +4,26 @@
  * See LICENSE in project root for more details.
 **/
 module.exports = function(arguments) {
-  console.log("Hi, I'm the add command.");
+  var fs = require('fs'),
+      filePath = arguments[0];
+
+  if (filePath === undefined) {
+    console.error("You are required to provide a valid file path to the add command.");
+    return;
+  }
+
+  try {
+    var stat = fs.statSync(filePath);
+
+    if (! stat.isFile()) {
+      console.error("The name you provide must be a file that exists, and may not be a directory.");
+      return;
+    }
+  } catch(error) {
+    console.error("Couldn't stat the file you tried to add.");
+    console.error(error.toString());
+    return;
+  }
+
+  fs.appendFileSync("deploygoonfiles.config", "\n" + filePath);
 }
