e2757642c9ccf65ad70f025ccec79a789d48b35e
Matt Farmer <matt@frmr.me>
2013-11-16 14:01:37 -0500

    Start implementing DeployJob spec.

--- test/util/deploy-job-spec.js
+++ test/util/deploy-job-spec.js
@@ -1,24 +1,77 @@
-var assert = require('assert');
-
 describe('DeployJob', function() {
+  var assert = require('assert'),
+      DeployJob = require("../../src/util/deploy-job"),
+      exampleJobDescriptor = {
+        "slug": "example-job",
+        "description": "An example job.",
+        "ipWhitelist": ["127.0.0.1"],
+        "deployActions": [
+          {
+            "name": "Echo Lo",
+            "command": "whoami"
+          }
+        ]
+      },
+      exampleJob = new DeployJob(exampleJobDescriptor);
+
+  var trustProxyJobDescriptor = {
+        "slug": "example-job",
+        "description": "An example job.",
+        "ipWhitelist": ["127.0.0.1"],
+        "trustProxy": true,
+        "deployActions": [
+          {
+            "name": "Echo Lo",
+            "command": "whoami"
+          }
+        ]
+      },
+      exampleJobWithProxyTrust = new DeployJob(trustProxyJobDescriptor);
+
   describe('#constructor', function() {
-    it('should construct a valid DeployJob for valid input');
+    it('should construct a valid DeployJob for valid input', function() {
+      assert.equal(typeof exampleJob, "object");
+    });
 
     it('should return a helpful error if required fields are not specified');
   });
 
   describe('#getSlug', function() {
-    it('should return the correct slug');
+    it('should return the correct slug', function() {
+      assert.equal(exampleJob.getSlug(), exampleJobDescriptor.slug);
+    });
   });
 
   describe('#getDescription', function() {
-    it('should return the correct description');
+    it('should return the correct description', function() {
+      assert.equal(exampleJob.getDescription(), exampleJobDescriptor.description);
+    });
   });
 
   describe('#isIpWhitelisted', function() {
-    it('should return true if the provided IP is whitelisted');
+    it('should return true if the provided IP is whitelisted', function() {
+      var ipWhitelistedResult = exampleJob.isIpWhitelisted("127.0.0.1");
 
-    it('should return false if the provided IP is not whitelisted');
+      assert.equal(ipWhitelistedResult, true);
+    });
+
+    it('should return false if the provided IP is not whitelisted', function() {
+      var ipWhitelistedResult = exampleJob.isIpWhitelisted("192.168.1.1");
+
+      assert.equal(ipWhitelistedResult, false);
+    });
+
+    it('should return false if proxy trust is disabled and the actual IP it not whitelisted', function() {
+      var ipWhitelistedResult = exampleJob.isIpWhitelisted("192.168.1.1", "127.0.0.1");
+
+      assert.equal(ipWhitelistedResult, false);
+    });
+
+    it('should return true if proxy trust is enabled and the forwarded for IP is whitelisted', function() {
+      var ipWhitelistedResult = exampleJobWithProxyTrust.isIpWhitelisted("192.168.1.1", "127.0.0.1");
+
+      assert.equal(ipWhitelistedResult, true);
+    });
   });
 
   describe("#executeDeployment", function() {
